4453f88f76606a69058ae2a54ca19b6ed797b2f4
Jenny <32821331+jenny-s51@users.noreply.github.com>
2024-01-19 11:30:23 -0500

    WIP card updates, align with Jeff card styling

--- frontend/src/pages/projects/screens/detail/notebooks/NotebookList.tsx
+++ frontend/src/pages/projects/screens/detail/notebooks/NotebookList.tsx
@@ -1,15 +1,17 @@
 import * as React from 'react';
-import { Button, Flex, FlexItem, Popover } from '@patternfly/react-core';
+import { Button, Popover } from '@patternfly/react-core';
 import { useNavigate } from 'react-router-dom';
 import { OutlinedQuestionCircleIcon } from '@patternfly/react-icons';
-import EmptyDetailsList from '~/pages/projects/screens/detail/EmptyDetailsList';
 import { ProjectSectionID } from '~/pages/projects/screens/detail/types';
 import DetailsSection from '~/pages/projects/screens/detail/DetailsSection';
 import { ProjectSectionTitles } from '~/pages/projects/screens/detail/const';
 import { ProjectDetailsContext } from '~/pages/projects/ProjectDetailsContext';
 import { FAST_POLL_INTERVAL } from '~/utilities/const';
 import DashboardPopupIconButton from '~/concepts/dashboard/DashboardPopupIconButton';
-import NotebookTable from './NotebookTable';
+import { useAccessReview } from '~/api';
+import { AccessReviewResource } from '~/pages/projects/screens/detail/const';
+import NotebookCardEmpty from "./NotebookCardEmpty";
+import NotebookTable from '~/pages/projects/screens/detail/notebooks/NotebookTable';
 
 const NotebookList: React.FC = () => {
   const {
@@ -20,6 +22,12 @@ const NotebookList: React.FC = () => {
   const navigate = useNavigate();
   const projectName = currentProject.metadata.name;
   const isNotebooksEmpty = notebookStates.length === 0;
+
+  const [allowCreate, rbacLoaded] = useAccessReview({
+    ...AccessReviewResource,
+    namespace: currentProject.metadata.name,
+  });
+
   const icon = (
     <img
       style={{
@@ -44,6 +52,7 @@ const NotebookList: React.FC = () => {
     <>
       <DetailsSection
         icon={icon}
+        // className="odh-project-details"
         id={ProjectSectionID.WORKBENCHES}
         title={ProjectSectionTitles[ProjectSectionID.WORKBENCHES] || ''}
         popover={
@@ -75,25 +84,7 @@ const NotebookList: React.FC = () => {
         isLoading={!loaded}
         loadError={loadError}
         isEmpty={isNotebooksEmpty}
-        emptyState={
-          <Flex>
-            <FlexItem>
-              <EmptyDetailsList
-                description="Creating a workbench allows you to add a Jupyter notebook to your project."
-                actions={[
-                  <Button
-                    key={`action-${ProjectSectionID.WORKBENCHES}`}
-                    onClick={() => navigate(`/projects/${projectName}/spawner`)}
-                    variant="secondary"
-                    size="lg"
-                  >
-                    Create workbench
-                  </Button>,
-                ]}
-              />
-            </FlexItem>
-          </Flex>
-        }
+        emptyState={<NotebookCardEmpty allowCreate={rbacLoaded && allowCreate} />}
       >
         <NotebookTable notebookStates={notebookStates} refresh={refresh} />
       </DetailsSection>
