ccb8544131881513e702e1eca8f88bc9bd6333d4
Jeffrey Phillips <jephilli@redhat.com>
2024-01-25 10:01:39 -0500

    Change workbench status order

--- frontend/src/pages/pipelines/global/runs/GlobalPipelineRuns.tsx
+++ frontend/src/pages/pipelines/global/runs/GlobalPipelineRuns.tsx
@@ -1,23 +1,53 @@
 import * as React from 'react';
+import { Flex, FlexItem } from '@patternfly/react-core';
+import { ArrowRightIcon } from '@patternfly/react-icons';
 import {
   pipelineRunsPageDescription,
   pipelineRunsPageTitle,
 } from '~/pages/pipelines/global/runs/const';
 import PipelineCoreApplicationPage from '~/pages/pipelines/global/PipelineCoreApplicationPage';
 import EnsureAPIAvailability from '~/concepts/pipelines/EnsureAPIAvailability';
+import { useAppSelector } from '~/redux/hooks';
 import GlobalPipelineRunsTabs from './GlobalPipelineRunsTabs';
 
-const GlobalPipelineRuns: React.FC = () => (
-  <PipelineCoreApplicationPage
-    title={pipelineRunsPageTitle}
-    description={pipelineRunsPageDescription}
-    getRedirectPath={(namespace) => `/pipelineRuns/${namespace}`}
-    overrideChildPadding
-  >
-    <EnsureAPIAvailability>
-      <GlobalPipelineRunsTabs />
-    </EnsureAPIAvailability>
-  </PipelineCoreApplicationPage>
-);
+const GlobalPipelineRuns: React.FC = () => {
+  const alternateUI = useAppSelector((state) => state.alternateUI);
+  return (
+    <PipelineCoreApplicationPage
+      title={
+        alternateUI ? (
+          <Flex
+            spaceItems={{ default: 'spaceItemsSm' }}
+            alignItems={{ default: 'alignItemsCenter' }}
+          >
+            <FlexItem>
+              <div
+                style={{
+                  background: 'rgba(87, 82, 209, 0.1)',
+                  borderRadius: 20,
+                  padding: 4,
+                  width: 40,
+                  height: 40,
+                }}
+              >
+                <ArrowRightIcon style={{ width: 32, height: 32 }} />
+              </div>
+            </FlexItem>
+            <FlexItem>{pipelineRunsPageTitle}</FlexItem>
+          </Flex>
+        ) : (
+          pipelineRunsPageTitle
+        )
+      }
+      description={pipelineRunsPageDescription}
+      getRedirectPath={(namespace) => `/pipelineRuns/${namespace}`}
+      overrideChildPadding
+    >
+      <EnsureAPIAvailability>
+        <GlobalPipelineRunsTabs />
+      </EnsureAPIAvailability>
+    </PipelineCoreApplicationPage>
+  );
+};
 
 export default GlobalPipelineRuns;
--- frontend/src/pages/projects/notebook/ListNotebookState.tsx
+++ frontend/src/pages/projects/notebook/ListNotebookState.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Skeleton } from '@patternfly/react-core';
 import CanEnableElyraPipelinesCheck from '~/concepts/pipelines/elyra/CanEnableElyraPipelinesCheck';
+import { useAppSelector } from '~/redux/hooks';
 import { NotebookState } from './types';
 import NotebookRouteLink from './NotebookRouteLink';
 import NotebookStatusToggle from './NotebookStatusToggle';
@@ -20,6 +21,8 @@ const ListNotebookState: React.FC<ListNotebookStateProps> = ({
   error,
   namespace,
 }) => {
+  const alternateUI = useAppSelector((state) => state.alternateUI);
+
   if (!loaded) {
     return <Skeleton />;
   }
@@ -36,16 +39,31 @@ const ListNotebookState: React.FC<ListNotebookStateProps> = ({
     <CanEnableElyraPipelinesCheck namespace={namespace}>
       {(canEnablePipelines) => (
         <div className="odh-list-notebook-state">
-          {notebookStates.map((state) => (
-            <React.Fragment key={state.notebook.metadata.name}>
-              <NotebookRouteLink className="odh-list-notebook-state__notebook" notebook={state.notebook} isRunning={state.isRunning} />
-              <NotebookStatusToggle
-                notebookState={state}
-                doListen
-                enablePipelines={canEnablePipelines}
-              />
-            </React.Fragment>
-          ))}
+          {notebookStates.map((state) =>
+            alternateUI ? (
+              <React.Fragment key={state.notebook.metadata.name}>
+                <NotebookRouteLink
+                  className="odh-list-notebook-state__notebook"
+                  notebook={state.notebook}
+                  isRunning={state.isRunning}
+                />
+                <NotebookStatusToggle
+                  notebookState={state}
+                  doListen
+                  enablePipelines={canEnablePipelines}
+                />
+              </React.Fragment>
+            ) : (
+              <React.Fragment key={state.notebook.metadata.name}>
+                <NotebookStatusToggle
+                  notebookState={state}
+                  doListen
+                  enablePipelines={canEnablePipelines}
+                />
+                <NotebookRouteLink notebook={state.notebook} isRunning={state.isRunning} />
+              </React.Fragment>
+            ),
+          )}
         </div>
       )}
     </CanEnableElyraPipelinesCheck>
